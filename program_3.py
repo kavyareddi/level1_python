911a6b221216f1d8f22933ba1a2a351bd151762a
kavyareddi <40757583+kavyareddi@users.noreply.github.com>
2018-09-28 10:44:41 +0530

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,18 +1,15 @@
 #Program to calculate credit card balance after 1 year
 #input :
-def calculate_CreditCardBalance(balance, annualInterestRate, monthlyPaymentRate):
+try:
+    balance = int(input('enter balance:'))
+    annualInterestRate = int(input('enter annualInterestRate :'))
+    monthlyPaymentRate = int(input('monthlyPaymentRate:'))
 
     totalPaid = 0
 
     for thisMonth in range(1,13):
         monthly_Interest_Rate = annualInterestRate / 12
-        try:
-            monthly_Interest_Rate = annualInterestRate / 0
-            print(monthly_Interest_Rate)
-        except:
-            print('bug')
-        print('outside')
-
+        
         minimum_Monthly_Payment = round(monthlyPaymentRate * balance, 2)
         monthly_Unpaid_Balance = balance - minimum_Monthly_Payment
         updated_Balance_EachMonth = round(monthly_Unpaid_Balance + (monthly_Interest_Rate * monthly_Unpaid_Balance) , 2)
@@ -26,7 +23,8 @@ def calculate_CreditCardBalance(balance, annualInterestRate, monthlyPaymentRate)
 
     print('Total paid: ' +str(totalPaid))
     print('Remaining balance: '+str(balance))
+except:
+    print(bug)
 
 
-calculate_CreditCardBalance(100, 0.2, 0.04)
 
