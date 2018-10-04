d1a34aa211887cd4322993ca55ff2679efada71a
kavyareddi <40757583+kavyareddi@users.noreply.github.com>
2018-10-04 17:13:06 +0530

    Update simple_calculator_classes.py

--- simple_calculator_classes.py
+++ simple_calculator_classes.py
@@ -1,31 +1,37 @@
 #simple calculator using classes
-class cal():
-    def __init__(self, a, b):
+class cal():#defining class
+    def __init__(self, a, b):#intializing
         self.a = a
         self.b = b
-
+#Methods for adding, substracting, multiplying, dividing two numbers and returning their respective results
     def add(self):
         return self.a + self.b
-
     def mul(self):
         return self.a * self.b
-
     def div(self):
-        return self.a / self.b
-
+        try:
+            return self.a / self.b
+        except:
+            print('value error')
     def sub(self):
         return self.a - self.b
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(float("Enter first number: "))
+b = int(float("Enter second number: "))
+#object for the class is created with the two numbers taken from the user passed as parameters.
 obj = cal(a, b)
 choice = 1
+#if choice is 0, the loop is exited
 while choice != 0:
     print("0. Exit")
     print("1. Add")
     print("2. Subtraction")
     print("3. Multiplication")
     print("4. Division")
-    choice = int(input("Enter choice: "))
+    try:
+        choice = int(input("Enter choice: "))#user input for preferred operation
+    except:
+        print('please enter a valid choice')
+    # loops for every operation of choice
     if choice == 1:
         print("Result: ", obj.add())
     elif choice == 2:
@@ -40,3 +46,7 @@ while choice != 0:
         print("choice error!")
 
 print()
+
+
+
+
